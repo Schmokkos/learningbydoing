f2621620cad11534454a89b23f3b97726e9d8e05
Joakim Schweizer <joakim.schweizer@nav.no>
2019-02-14 14:17:01 +0100

    Lagt til enhetstester

---
+++ src/test/java/no/schweizer/QuestionTest.java
@@ -0,0 +1,17 @@
+package no.schweizer;
+
+import no.schweizer.controller.QuestionController;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuestionTest {
+
+    private QuestionController qc = new QuestionController();
+
+    @Test
+    public void sjekkCreateWord(){
+        Assert.assertNotNull(qc.createWord(5));
+        Assert.assertNotNull(qc.createWord(6));
+        Assert.assertNotNull(qc.createWord(7));
+    }
+}
--- src/test/java/no/schweizer/ResultatTest.java
+++ src/test/java/no/schweizer/ResultatTest.java
@@ -6,18 +6,16 @@ import org.junit.Test;
 
 public class ResultatTest {
 
+    private ResultatController rc = new ResultatController();
+
     @Test
     public void sjekkProsenter(){
-        ResultatController rc = new ResultatController();
-
         double v = rc.prosentResultat(37, 41);
         Assert.assertEquals(90.2439, v, 0.00001);
     }
 
     @Test
     public void sjekkKarakter(){
-        ResultatController rc = new ResultatController();
-
         Assert.assertEquals('A', rc.gradeResult(95.0));
         Assert.assertEquals('B', rc.gradeResult(80.0));
         Assert.assertEquals('C', rc.gradeResult(65.0));
@@ -28,9 +26,40 @@ public class ResultatTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void sjekkKarakterFeil(){
-        ResultatController rc = new ResultatController();
-
         rc.gradeResult(Double.NaN);
+    }
+
+    @Test
+    public void sjekkIsItSolved(){
+        boolean[] array = new boolean[3];
+        Assert.assertFalse(rc.isItSolved(array));
+        array[0] = true;
+        Assert.assertFalse(rc.isItSolved(array));
+        array[1] = true;
+        Assert.assertFalse(rc.isItSolved(array));
+        array[2] = true;
+        Assert.assertTrue(rc.isItSolved(array));
+    }
+
+    @Test
+    public void sjekkIsLetterInWord(){
+        char[] array = {'a','b','c'};
+        Assert.assertFalse(rc.isLetterInWord('g', array));
+        Assert.assertFalse(rc.isLetterInWord('o', array));
+        Assert.assertFalse(rc.isLetterInWord('i', array));
+        Assert.assertTrue(rc.isLetterInWord('a', array));
+        Assert.assertTrue(rc.isLetterInWord('b', array));
+        Assert.assertTrue(rc.isLetterInWord('c', array));
+    }
+
+    @Test
+    public void sjekkAreYouDead(){
+        Assert.assertFalse(rc.areYouDead(0,5));
+        Assert.assertFalse(rc.areYouDead(3,5));
+        Assert.assertFalse(rc.areYouDead(5,8));
+        Assert.assertTrue(rc.areYouDead(5,3));
+        Assert.assertTrue(rc.areYouDead(5,5));
+        Assert.assertTrue(rc.areYouDead(20,10));
     }
 
 }
