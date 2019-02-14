a761a148f87de21be0ad0a5aea726325b5970290
Joakim Schweizer <joakim.schweizer@nav.no>
2019-02-14 13:16:06 +0100

    Fixed AppSelector with input.

--- src/main/java/no/schweizer/AppSelector.java
+++ src/main/java/no/schweizer/AppSelector.java
@@ -1,8 +1,25 @@
 package no.schweizer;
 
+import java.util.Scanner;
+
 public class AppSelector {
+    private static Scanner scan = new Scanner(System.in);
     public static void main(String[] args){
-        //new MathTester();
-        new HangMan();
+        System.out.println("Please select what application you would like to launch");
+        System.out.println("1 - MathTester");
+        System.out.println("2 - HangMan");
+        System.out.println("Anything else - Exit");
+        System.out.print("Input app number:");
+        int input = scan.nextInt();
+
+        if (input == 1){
+            new MathTester();
+        }
+        if (input == 2){
+            new HangMan();
+        }
+        else {
+            System.out.println("Bye then...");
+        }
     }
 }
